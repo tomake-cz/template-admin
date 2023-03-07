fcb07af432efde664547c252d757fa507648ee30
PatrikHajek <hajan21@seznam.cz>
2023-03-07 16:17:19 +0100

    link hover

--- frontend/src/components/nav/NavLink.vue
+++ frontend/src/components/nav/NavLink.vue
@@ -11,15 +11,31 @@ const props = defineProps<{
   };
 }>();
 const { title, url, icon } = toRefs(props.link);
+
+const route = useRoute();
+const isActive = computed(() => route.path === url.value);
+const isHover = ref(false);
 </script>
 
 <template>
-  <NuxtLink :to="url" class="flex rounded-xl p-5 hover:bg-black-dark">
+  <NuxtLink
+    :to="url"
+    :class="{ 'bg-black-dark': isActive }"
+    class="flex rounded-xl p-5"
+    @mouseenter="isHover = true"
+    @mouseleave="isHover = false"
+  >
     <img
       :src="useAsset(icon.url)"
       :alt="icon.alt"
-      class="icon-gray-dark mr-2 h-5 w-5"
+      :class="[isHover ? 'icon-gray-lighter' : 'icon-gray-dark']"
+      class="mr-2 h-5 w-5"
     />
-    <span class="text-sm text-gray-dark">{{ title }}</span>
+    <span
+      :class="[isHover ? 'text-gray-lighter' : 'text-gray-dark']"
+      class="text-sm"
+    >
+      {{ title }}
+    </span>
   </NuxtLink>
 </template>
