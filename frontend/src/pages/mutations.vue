a5d7c06cc3b4335ede4b89d3c77c96cd572083e9
PatrikHajek <hajan21@seznam.cz>
2023-03-08 19:33:01 +0100

    reworked mutations

--- frontend/src/pages/mutations.vue
+++ frontend/src/pages/mutations.vue
@@ -9,7 +9,15 @@ const query = gql`
     }
   }
 `;
-const { result, loading } = useQuery(query);
+type Query = {
+  single: {
+    id: string;
+    name: string;
+    text: string;
+    number: number;
+  };
+};
+const { result } = useQuery<Query>(query);
 
 const mutation = gql`
   mutation updateSingle($name: String, $text: String, $number: Int) {
@@ -26,32 +34,31 @@ const mutation = gql`
     }
   }
 `;
-const form = ref<HTMLElement | null>(null);
+
+const getValues = () => {
+  const inputs = document
+    .querySelector('#mutations')
+    ?.querySelectorAll('input');
+  return [...(inputs ?? [])].map((input) => input.value);
+};
+
 const send = () => {
+  const values = getValues();
+
   const { mutate, onDone, onError } = useMutation(mutation, {
     variables: {
-      name: (
-        form?.value?.querySelector('input[name="name"]') as HTMLInputElement
-      )?.value,
-      text: (
-        form?.value?.querySelector('input[name="email"]') as HTMLInputElement
-      )?.value,
-      number: parseInt(
-        (form?.value?.querySelector('input[name="number"]') as HTMLInputElement)
-          ?.value,
-      ),
+      name: values[0],
+      text: values[1],
+      number: parseInt(values[2]),
     },
   });
+
   console.log(
-    (form?.value?.querySelector('input[name="name"]') as HTMLInputElement)
-      ?.value,
-    (form?.value?.querySelector('input[name="email"]') as HTMLInputElement)
-      ?.value,
-    parseInt(
-      (form?.value?.querySelector('input[name="number"]') as HTMLInputElement)
-        ?.value,
-    ),
+    values[0],
+    values[1],
+    'number: ' + parseInt(values[2] + values[2]),
   );
+
   mutate();
   onDone((data) => {
     console.log(data);
@@ -65,29 +72,27 @@ const send = () => {
 </script>
 
 <template>
-  <div class="mt-20 flex flex-col items-center">
-    <h1 class="mb-8 text-2xl">Mutations</h1>
-    <form ref="form">
-      <AppInputText
-        label="Jméno"
-        :value="result?.single?.name"
-        name="name"
-        :max-length="30"
-      />
-      <AppInputText
-        label="Email"
-        info="Zadejte váš email"
-        :value="result?.single?.text"
-        name="email"
-        :max-length="30"
-      />
-      <AppInputText
-        label="Číslo"
-        :value="result?.single?.number"
-        name="number"
-        :max-length="10"
-      />
-      <AppButtonSend class="mt-3" @click="send" />
-    </form>
-  </div>
+  <AppRecord id="mutations">
+    <AppInputText
+      label="Jméno"
+      :value="result?.single?.name"
+      name="name"
+      :max-length="30"
+    />
+    <AppInputText
+      label="Email"
+      info="Zadejte váš email"
+      :value="result?.single?.text"
+      name="email"
+      :max-length="30"
+    />
+    <AppInputText
+      label="Číslo"
+      :value="result?.single?.number"
+      name="number"
+      :max-length="10"
+      pattern="[0-9]*"
+    />
+    <AppButtonSend class="mt-3" @click="send" />
+  </AppRecord>
 </template>
