68054ee0d7d5627e2a5eab58c255c9a11b290f1d
PatrikHajek <hajan21@seznam.cz>
2023-03-08 19:32:37 +0100

    dynamic title from url

--- frontend/src/components/app/record/AppRecord.vue
+++ frontend/src/components/app/record/AppRecord.vue
@@ -1,11 +1,15 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const route = useRoute();
+const path = route.path.split('/')[1];
+const title = path.slice(0, 1).toUpperCase() + path.slice(1);
+</script>
 
 <template>
   <div class="flex h-full justify-between">
     <AppNav />
     <div class="grow py-12 px-6">
       <div class="mb-12">
-        <h1 class="text-3xl">Hlavička</h1>
+        <h1 class="text-3xl">{{ title }}</h1>
         <AppRevisionHistory />
       </div>
       <div class="flex flex-col gap-6">
