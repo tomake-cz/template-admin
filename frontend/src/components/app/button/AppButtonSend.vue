10d4d9842c161765dee66020d89cba3af7837186
PatrikHajek <hajan21@seznam.cz>
2023-03-08 19:32:52 +0100

    changed colors temporarily

--- frontend/src/components/app/button/AppButtonSend.vue
+++ frontend/src/components/app/button/AppButtonSend.vue
@@ -5,7 +5,7 @@ defineEmits(['click']);
 <template>
   <button
     type="button"
-    class="rounded bg-blue-500 py-2 px-4 font-bold text-white hover:bg-blue-700"
+    class="rounded bg-site-1 py-2 px-4 font-bold text-white hover:bg-site-2"
     @click="$emit('click')"
   >
     Odeslat
