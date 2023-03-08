ab2a93fa10e017d34c7152bf64ba877cc1144754
PatrikHajek <hajan21@seznam.cz>
2023-03-08 14:26:55 +0100

    renamed

--- frontend/src/components/header-bar/HeaderBar.vue
+++ frontend/src/components/header-bar/HeaderBar.vue
@@ -1,10 +1,8 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="flex h-16 w-full items-center pr-6">
-    <div class="flex grow items-center justify-center pl-[74px]">
-      <HeaderBarTitle />
-    </div>
-    <HeaderBarAccount />
+  <div>
+    <HeaderBarMain />
+    <HeaderBarSub />
   </div>
 </template>
