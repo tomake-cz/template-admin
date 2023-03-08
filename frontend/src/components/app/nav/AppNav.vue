7c9f788d8be972ca95033060eae2d5db66ba1a82
PatrikHajek <hajan21@seznam.cz>
2023-03-08 18:04:10 +0100

    app nav

---
+++ frontend/src/components/app/nav/AppNav.vue
@@ -0,0 +1,52 @@
+<script setup lang="ts">
+const groups: {
+  id: number;
+  title: string;
+  links: {
+    id: number;
+    title: string;
+    url: string;
+  }[];
+}[] = [
+  {
+    id: 0,
+    title: 'Hlavní strana',
+    links: [
+      {
+        id: 0,
+        title: 'Hlavička',
+        url: '',
+      },
+      {
+        id: 1,
+        title: 'Proč FVE od nás?',
+        url: '',
+      },
+    ],
+  },
+  {
+    id: 1,
+    title: 'Portfolio',
+    links: [
+      {
+        id: 0,
+        title: 'Hlavička',
+        url: '',
+      },
+      {
+        id: 1,
+        title: 'Galerie',
+        url: '',
+      },
+    ],
+  },
+];
+</script>
+
+<template>
+  <nav class="h-full w-60 bg-gray-dark px-6 py-12">
+    <template v-for="group in groups" :key="group.id + group.title">
+      <AppNavGroup :group="group" />
+    </template>
+  </nav>
+</template>
---
+++ frontend/src/components/app/nav/AppNavLink.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+const props = defineProps<{
+  link: {
+    title: string;
+    url: string;
+  };
+}>();
+const { title, url } = toRefs(props.link);
+</script>
+
+<template>
+  <NuxtLink :to="url" class="text-gray-darker">{{ title }}</NuxtLink>
+</template>
