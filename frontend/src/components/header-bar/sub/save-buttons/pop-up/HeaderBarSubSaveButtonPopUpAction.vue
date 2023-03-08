8ca1c84dddb33339626b08c17a8840e86fb5db33
PatrikHajek <hajan21@seznam.cz>
2023-03-08 18:18:32 +0100

    fixed popup design

--- frontend/src/components/header-bar/sub/save-buttons/pop-up/HeaderBarSubSaveButtonPopUpAction.vue
+++ frontend/src/components/header-bar/sub/save-buttons/pop-up/HeaderBarSubSaveButtonPopUpAction.vue
@@ -13,12 +13,12 @@ defineEmits(['close']);
 <template>
   <button
     :class="{
-      'rounded-b-xl border-t border-gray-darker': isLast,
+      'rounded-b-xl border-t-2 border-solid border-gray-lighter': isLast,
     }"
     class="flex h-[46px] items-center justify-between px-4 text-xs first:rounded-xl"
     @click="$emit('close')"
   >
-    <span :class="[isLast ? 'text-green-500' : 'text-black-light']">
+    <span :class="[isLast ? 'text-site-1' : 'text-gray-lighter']">
       {{ action.text }}
     </span>
     <span
