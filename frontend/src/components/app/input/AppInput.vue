45088dc75ecd6a81c9edb7de38468aebbbae048f
PatrikHajek <hajan21@seznam.cz>
2023-03-09 17:05:33 +0100

    made smaller label

--- frontend/src/components/app/input/AppInput.vue
+++ frontend/src/components/app/input/AppInput.vue
@@ -1,18 +1,17 @@
 <script setup lang="ts">
 defineProps<{
-  label: {
-    text?: string;
-    name?: string;
-  };
+  label: string;
+  name?: string;
   info?: string;
+  isSmaller: boolean;
 }>();
 </script>
 
 <template>
   <div class="mb-2 flex flex-col">
-    <label :for="label.name" class="mb-2 text-lg font-bold uppercase">{{
-      label.text
-    }}</label>
+    <label :for="name" :class="{ 'text-sm': isSmaller }" class="mb-2 uppercase">
+      {{ label }}
+    </label>
     <p v-if="info" class="mb-2 text-sm font-normal">{{ info }}</p>
     <slot />
   </div>
