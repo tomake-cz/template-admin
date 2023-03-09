5de73d4268fc5697525afaa94a094d45576b9331
PatrikHajek <hajan21@seznam.cz>
2023-03-09 17:10:37 +0100

    input date without much design

---
+++ frontend/src/components/app/input/AppInputDate.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+const props = defineProps<{
+  label: string;
+  info?: string;
+  value?: string;
+  name?: string;
+  isSmaller?: boolean;
+}>();
+const { label } = toRefs(props);
+</script>
+
+<template>
+  <AppInput :label="label" :info="info" :name="name" :is-smaller="isSmaller">
+    <input type="datetime-local" class="text-xs font-normal" />
+  </AppInput>
+</template>
